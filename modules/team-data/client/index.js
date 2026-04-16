01fd56add72aa52dfb801b0c3fa3ef3963b5a73d
Dipanshu Gupta <dipgupta@redhat.com>
2026-04-16 23:47:47 +0530

    chore: remove modules/team-data directory
    All team-data functionality (IPA registry, identity management,
    lifecycle tracking, org explorer) has been absorbed into team-tracker
    and shared/server/roster-sync/. The module directory is no longer
    needed.
    
    Storage keys (team-data/registry.json, etc.) are preserved as-is
    on the PVC -- only the code directory is removed.

--- modules/team-data/client/index.js
+++
@@ -1,7 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const routes = {
-  'people': defineAsyncComponent(() => import('./views/PeopleDirectoryView.vue')),
-  'person-detail': defineAsyncComponent(() => import('./views/PersonDetailView.vue')),
-  'org-explorer': defineAsyncComponent(() => import('./views/OrgExplorerView.vue')),
-}
